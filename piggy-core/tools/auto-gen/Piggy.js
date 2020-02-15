cbc5f7af9a8d565971265cc93a2741598b69c2d1
jiangliang <jl88744653@gmail.com>
2020-02-15 15:09:55 +0800

    piggy tool

--- piggy-core/tools/auto-gen/Piggy.js
+++ piggy-core/tools/auto-gen/Piggy.js
@@ -1,2 +1,27 @@
-const fs = require("fs");
-console.log(process.argv);
+const Promise = require("bluebird"),
+  path = require("path"),
+  fs = require("fs");
+
+Promise.promisifyAll(fs);
+
+class Piggy {
+  constructor() {}
+}
+
+console.log(process.argv, process.argv.length);
+if (process.argv.length <= 2) {
+  return console.warn("请提供一个创建项目的路径!");
+}
+
+let target_path = process.argv[2];
+fs.exists(target_path, exists => {
+  if (exists) {
+    return console.log("请提供一个空目录");
+  }
+  target_path = path.resolve(target_path);
+  fs.mkdirAsync(target_path, { recursive: true }).then(err => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
