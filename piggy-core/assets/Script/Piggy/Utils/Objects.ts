273d3392c78d52e344aea3ce8ceb0fd96564db21
jiangliang <jl88744653@gmail.com>
2020-02-26 18:16:30 +0800

    [add] Objects.ts
    对象库

---
+++ piggy-core/assets/Script/Piggy/Utils/Objects.ts
@@ -0,0 +1,26 @@
+/**
+ * @file Objects
+ * @description 对象库
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
+namespace Objects {
+  /**
+   * 简单拷贝
+   * @param obj 对象或其他数据
+   */
+  export function clone(obj: any) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+}
+
+export { Objects as objects };
