abd840b2a24753ad841ae6abeca38bf0e76ee868
jiangliang <jl88744653@gmail.com>
2020-02-27 16:33:05 +0800

    [add] IdGenerator.ts
    新增Id生成器

---
+++ piggy-core/assets/Script/Piggy/Core/IdGenerator.ts
@@ -0,0 +1,43 @@
+/**
+ * @file IdGenerator
+ * @description Id生成器
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
+class IdGenerator {
+  private id: number = 0;
+  private category: string = "";
+
+  /**
+   * Id生成器构造器
+   * @param category 类型
+   */
+  constructor(category: string) {
+    this.category = category || "default";
+    this.id = 0 | (Math.random() * 998);
+  }
+
+  /**
+   * 获取新的id
+   */
+  next() {
+    return this.category + "." + ++this.id;
+  }
+}
+
+export namespace ids {
+  export const global = new IdGenerator("global");
+  export const event = new IdGenerator("event");
+  export const timer = new IdGenerator("timer");
+  export const fsm = new IdGenerator("fsm");
+  export const pool = new IdGenerator("pool");
+}
