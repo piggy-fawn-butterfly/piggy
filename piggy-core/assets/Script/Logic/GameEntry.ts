b21e0fcb5c91fbe2de916376ece128f916b66a89
jiangliang <jl88744653@gmail.com>
2020-02-27 11:08:56 +0800

    [add] 新增音乐播放

--- piggy-core/assets/Script/Logic/GameEntry.ts
+++ piggy-core/assets/Script/Logic/GameEntry.ts
@@ -1,6 +1,10 @@
 import { app } from "../Piggy/Core/App";
+import { res } from "../Piggy/Core/Res";
 import { pool } from "../Piggy/Core/Pool";
-import { assetPath } from "../Piggy/Const/AssetPath";
+import { sound } from "../Piggy/Core/Sound";
+import { logger } from "../Piggy/Core/Logger";
+import { assets } from "../Piggy/Const/Assets";
+
 const { ccclass } = cc._decorator;
 
 /**
@@ -31,11 +35,27 @@ class GameEntry extends app {
    * 加载资源
    */
   async loadRes() {
-    await pool.load([[assetPath.Prefab_Testcase_CanvasAdapter, 1]]);
-    await pool.get(assetPath.Prefab_Testcase_CanvasAdapter).then(node => {
+    //你可以在一开始就加载所有需要用到的资源
+    //在加载进度回调中更新页面 Loading
+    //你也可以在运行时加载指定的资源
+    //这意味着注释下面这段代码并不会影响代码的运行
+    let resources = [
+      assets.Prefab_Testcase_CanvasAdapter,
+      assets.Sound_LoopingBgm1,
+      assets.Sound_ButtonClick
+    ];
+    await res.load(resources, (c, t) => {
+      logger.info("资源加载进度:", (((c / t) * 100) | 0) + "%");
+    });
+
+    //加载对象池
+    await pool.load([[assets.Prefab_Testcase_CanvasAdapter, 1]]);
+    await pool.get(assets.Prefab_Testcase_CanvasAdapter).then(node => {
       node && this.node.addChild(node);
     });
-    pool.dump();
+
+    //播放背景音乐
+    await sound.play(assets.Sound_LoopingBgm1, true, true);
   }
 }
 
