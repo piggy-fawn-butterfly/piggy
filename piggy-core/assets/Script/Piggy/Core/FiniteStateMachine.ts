1dab76e2d0f3bb3e1b8f7ae4ccaaff590ce0e7c5
jiangliang <jl88744653@gmail.com>
2020-02-28 15:41:59 +0800

    [modify] 修改有限状态机类型定义

--- piggy-core/assets/Script/Piggy/Core/FiniteStateMachine.ts
+++ piggy-core/assets/Script/Piggy/Core/FiniteStateMachine.ts
@@ -11,26 +11,12 @@ import { strings } from "../Utils/Strings";
  * @author DoooReyn <jl88744653@gmail.com>
  * @example
  * ```js
- * let game_state = fsm.create(
- *   "ready",
- *   ["ready", "running", "paused", "stopped"],
- *   [
- *     { name: "ready", next: ["running"] },
- *     { name: "running", next: ["paused", "stopped"] },
- *     { name: "paused", next: ["running"] },
- *     { name: "stopped", next: ["ready"] }
- *   ]
- * );
- * game_state.dump();
- * logger.info("canTransitTo ready:", game_state.canTransitTo("ready"));
- * logger.info("canTransitTo running:", game_state.canTransitTo("running"));
- * logger.info("canTransitTo paused:", game_state.canTransitTo("paused"));
- * logger.info("canTransitTo stopped:", game_state.canTransitTo("stopped"));
- * game_state.transitTo("running").dump();
- * logger.info("canTransitTo ready:", game_state.canTransitTo("ready"));
- * logger.info("canTransitTo running:", game_state.canTransitTo("running"));
- * logger.info("canTransitTo paused:", game_state.canTransitTo("paused"));
- * logger.info("canTransitTo stopped:", game_state.canTransitTo("stopped"));
+ * let machine = fsm.create(states.app);
+ * machine.dump();
+ * logger.info("canTransitTo ready:", machine.canTransitTo("running"));
+ * machine.transitTo("running").dump();
+ * logger.info("canTransitTo ready:", machine.canTransitTo("ready"));
+ * machine.dump();
  * ```
  * @license MIT
  * @identifier
@@ -47,29 +33,44 @@ export namespace fsm {
   /**
    * 状态机状态值
    */
-  export type T_FSM_STATE = string;
+  export type T_Fsm_State = string;
 
   /**
    * 状态机动作
    */
-  export type T_FSM_TRANSITION = string;
+  export type T_Fsm_Transition = string;
 
   /**
    * 状态机状态切换配置
    */
   export interface I_FSM_TRANSITION {
-    name: T_FSM_TRANSITION;
-    from: T_FSM_STATE;
-    to: T_FSM_STATE;
+    name: T_Fsm_Transition;
+    from: T_Fsm_State;
+    to: T_Fsm_State;
+  }
+
+  export interface I_Fsm_Transitions {
+    [key: string]: any;
+  }
+
+  export interface I_Fsm_States {
+    [key: string]: any;
+  }
+
+  export interface I_Fsm_Structure {
+    default_state: string;
+    states: string[];
+    transitions: I_FSM_TRANSITION[];
   }
 
   /**
    * 状态机配置
    */
   export interface I_Machine_Configuration {
-    default_state: string;
-    states: string[];
-    transitions: I_FSM_TRANSITION[];
+    name: string;
+    transition: I_Fsm_Transitions;
+    state: I_Fsm_States;
+    structure: I_Fsm_Structure;
   }
 
   /**
@@ -85,7 +86,7 @@ export namespace fsm {
    * @param configuration 状态机配置
    */
   export function isValidStateMachine(configuration: I_Machine_Configuration) {
-    const { default_state, states, transitions } = configuration;
+    const { default_state, states, transitions } = configuration.structure;
     states.length <= 1 && wrapErrorMsg(i18n.I.text(i18n.K.fsm_tip_1));
     !states.includes(default_state) &&
       wrapErrorMsg(i18n.I.text(i18n.K.fsm_tip_2));
@@ -98,20 +99,13 @@ export namespace fsm {
   }
 
   /**
-   * 状态机回调
-   */
-  interface I_StateMachine_CallFunc {
-    (machine: StateMachine): void;
-  }
-
-  /**
    * 创建一个状态机
    * @param configuration 状态机配置
    */
   export function create(
     configuration: I_Machine_Configuration
   ): Promise<StateMachine> {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       try {
         isValidStateMachine(configuration);
         let machine = new StateMachine(configuration);
@@ -130,7 +124,7 @@ export namespace fsm {
   export class StateMachine {
     public m_category: string;
     private m_configuration: I_Machine_Configuration;
-    private m_current_state: T_FSM_STATE;
+    private m_current_state: T_Fsm_State;
 
     /**
      * 状态机构造器
@@ -141,13 +135,20 @@ export namespace fsm {
     constructor(configuration: I_Machine_Configuration) {
       this.m_category = ids.fsm.next();
       this.m_configuration = configuration;
-      this.m_current_state = configuration.default_state;
+      this.m_current_state = configuration.structure.default_state;
+    }
+
+    /**
+     * 获得状态机名称
+     */
+    public name(): string {
+      return this.m_configuration.name;
     }
 
     /**
      * 获得当前状态
      */
-    current(): T_FSM_STATE {
+    public current(): T_Fsm_State {
       return this.m_current_state;
     }
 
@@ -155,7 +156,7 @@ export namespace fsm {
      * 判断当前状态
      * @param state 状态
      */
-    is(state: T_FSM_STATE): boolean {
+    public is(state: T_Fsm_State): boolean {
       return this.m_current_state === state;
     }
 
@@ -163,9 +164,10 @@ export namespace fsm {
      * 是否可以切换到指定状态
      * @param state 状态
      */
-    canTransitTo(transition: T_FSM_TRANSITION): I_FSM_TRANSITION {
-      for (let i = 0; i < this.m_configuration.transitions.length; i++) {
-        let t = this.m_configuration.transitions[i];
+    public canTransitTo(transition: T_Fsm_Transition): I_FSM_TRANSITION {
+      let transitions = this.m_configuration.structure.transitions;
+      for (let i = 0; i < transitions.length; i++) {
+        let t = transitions[i];
         if (t.name === transition && t.from === this.m_current_state) {
           return t;
         }
@@ -177,7 +179,7 @@ export namespace fsm {
      * 切换动作
      * @param transition 动作
      */
-    transitTo(transition: T_FSM_TRANSITION): StateMachine {
+    public transitTo(transition: T_Fsm_Transition): StateMachine {
       let target = this.canTransitTo(transition);
       if (!target) {
         let context = {
@@ -187,7 +189,7 @@ export namespace fsm {
         logger.error(strings.render(i18n.I.text(i18n.K.fsm_tip_6), context));
       } else {
         this.m_current_state = target.to;
-        events.dispatch(transition, target);
+        events.dispatch(this.m_category, target);
       }
       return this;
     }
@@ -195,26 +197,27 @@ export namespace fsm {
     /**
      * 重置
      */
-    reset(): StateMachine {
-      this.m_current_state = this.m_configuration.default_state;
+    public reset(): StateMachine {
+      this.m_current_state = this.m_configuration.structure.default_state;
       return this;
     }
 
     /**
      * 输出信息
      */
-    dump(): StateMachine {
+    public dump(): StateMachine {
       let info = i18n.I.text(i18n.K.fsm_tip_7);
       let label = strings.render(info, { category: this.m_category });
       let data = [
         i18n.I.text(i18n.K.fsm_tip_8) + this.m_current_state,
-        i18n.I.text(i18n.K.fsm_tip_9) + this.m_configuration.default_state,
+        i18n.I.text(i18n.K.fsm_tip_9) +
+          this.m_configuration.structure.default_state,
         i18n.I.text(i18n.K.fsm_tip_10),
-        this.m_configuration.states.join("\n"),
+        this.m_configuration.structure.states.join("\n"),
         i18n.I.text(i18n.K.fsm_tip_11)
       ];
-      this.m_configuration.transitions.forEach(transition => {
-        data.push(`${transition.name}: ${transition.from} => ${transition.to}`);
+      this.m_configuration.structure.transitions.forEach(transition => {
+        data.push(`${this.name()}: ${transition.from} => ${transition.to}`);
       });
       logger.info(label, ...data);
       return this;
