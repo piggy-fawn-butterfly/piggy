1dbc464b53fbd418b006f64fe40b1e8374a8228c
jiangliang <jl88744653@gmail.com>
2020-02-28 15:37:57 +0800

    [modify] 修改状态机状态配置

--- piggy-core/assets/Script/Piggy/Const/States.ts
+++ piggy-core/assets/Script/Piggy/Const/States.ts
@@ -17,15 +17,16 @@ export const states = {
   /**
    * 游戏状态机配置
    */
-  game: {
+  app: {
+    name: "app",
     transition: {
-      start: "game_start",
-      pause: "game_pause",
-      resume: "game_resume",
-      over: "game_over",
-      cheat: "game_cheat",
-      reset: "game_reset",
-      restart: "game_restart"
+      start: "tr_app_start",
+      pause: "tr_app_pause",
+      resume: "tr_app_resume",
+      over: "tr_app_over",
+      cheat: "tr_app_cheat",
+      reset: "tr_app_reset",
+      restart: "tr_app_restart"
     },
     state: {
       ready: "ready",
@@ -37,17 +38,17 @@ export const states = {
       default_state: "ready",
       states: ["ready", "running", "paused", "stopped"],
       transitions: [
-        { name: "game_start", from: "ready", to: "running" },
-        { name: "game_pause", from: "running", to: "paused" },
-        { name: "game_resume", from: "paused", to: "running" },
-        { name: "game_over", from: "running", to: "stopped" },
-        { name: "game_cheat", from: "ready", to: "ready" },
-        { name: "game_cheat", from: "running", to: "ready" },
-        { name: "game_cheat", from: "paused", to: "ready" },
-        { name: "game_cheat", from: "stopped", to: "ready" },
-        { name: "game_reset", from: "paused", to: "ready" },
-        { name: "game_reset", from: "stopped", to: "ready" },
-        { name: "game_restart", from: "stopped", to: "running" }
+        { name: "tr_app_start", from: "ready", to: "running" },
+        { name: "tr_app_pause", from: "running", to: "paused" },
+        { name: "tr_app_resume", from: "paused", to: "running" },
+        { name: "tr_app_over", from: "running", to: "stopped" },
+        { name: "tr_app_cheat", from: "ready", to: "ready" },
+        { name: "tr_app_cheat", from: "running", to: "ready" },
+        { name: "tr_app_cheat", from: "paused", to: "ready" },
+        { name: "tr_app_cheat", from: "stopped", to: "ready" },
+        { name: "tr_app_reset", from: "paused", to: "ready" },
+        { name: "tr_app_reset", from: "stopped", to: "ready" },
+        { name: "tr_app_restart", from: "stopped", to: "running" }
       ]
     }
   }
