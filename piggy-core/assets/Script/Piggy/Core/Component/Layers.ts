44fd03be0aa3660a9db85396e2df665dee938a07
jiangliang <jl88744653@gmail.com>
2020-03-02 17:24:07 +0800

    [add] Layers.ts
    - 新增Layers层管理器

---
+++ piggy-core/assets/Script/Piggy/Core/Component/Layers.ts
@@ -0,0 +1,22 @@
+/**
+ * @file Layers
+ * @description UI层管理器
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
+class Layers {
+  public static s_instance: Layers = new Layers();
+
+  private constructor() {}
+}
+
+export const layers = Layers.s_instance;
