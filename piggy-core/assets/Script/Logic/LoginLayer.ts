5067def9ef227de552abee1faf21aa722d304b23
jiangliang <jl88744653@gmail.com>
2020-03-03 11:43:57 +0800

    [modify] LoginLayer adjust original position

--- piggy-core/assets/Script/Logic/LoginLayer.ts
+++ piggy-core/assets/Script/Logic/LoginLayer.ts
@@ -2,6 +2,22 @@ import { layerBase } from "../Piggy/Core/Component/LayerBase";
 
 const { ccclass } = cc._decorator;
 
+/**
+ * @file LoginLayer
+ * @class
+ * @description 登录层
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
 @ccclass
 class LoginLayer extends layerBase {
   onLoad() {
