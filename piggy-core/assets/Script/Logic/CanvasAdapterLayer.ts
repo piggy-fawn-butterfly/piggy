d574ff0ecdb072072becec9d0524a53faebc2634
jiangliang <jl88744653@gmail.com>
2020-03-04 22:31:46 +0800

    [replace] Testcase_CanvasAdapter -> CanvasAdapterLayer

---
+++ piggy-core/assets/Script/Logic/CanvasAdapterLayer.ts
@@ -0,0 +1,21 @@
+import { layerBase } from "../Piggy/Core/Component/LayerBase";
+
+const { ccclass } = cc._decorator;
+
+/**
+ * @file CanvasAdapterLayer
+ * @description Canvas适配视图
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
+@ccclass
+class CanvasAdapterLayer extends layerBase {}
