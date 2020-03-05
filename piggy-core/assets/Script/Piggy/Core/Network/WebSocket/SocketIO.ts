59c1e87df08a4b36493800e49f9755a91929e4a1
jiangliang <jl88744653@gmail.com>
2020-03-05 23:18:59 +0800

    [add] SocketIO.ts
    新增 socket.io
    
    未完成

---
+++ piggy-core/assets/Script/Piggy/Core/Network/WebSocket/SocketIO.ts
@@ -0,0 +1,19 @@
+/**
+ * @file SocketIO
+ * @description SocketIO网络模块
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
+export class io_socket {
+  public static s_instance: io_socket = new io_socket();
+  private constructor() {}
+}
