4830b57a7d2086e2383204536ad5562c85670b56
jiangliang <jl88744653@gmail.com>
2020-03-05 23:19:59 +0800

    [add] HttpClient.ts
    新增 http-client
    
    未完成

---
+++ piggy-core/assets/Script/Piggy/Core/Network/Http/HttpClient.ts
@@ -0,0 +1,19 @@
+/**
+ * @file HttpClient
+ * @description HTTP网络模块
+ * @author DoooReyn <jl88744653@gmail.com>
+ * @license MIT
+ * @identifier
+ * ```
+ *             ╥━━━┳━━━━━━━━━╭━━╮━━━┳━━━╥
+ *             ╢━D━┣ ╭╮╭━━━━━┫┃▋▋━▅ ┣━R━╢
+ *             ╢━O━┣ ┃╰┫┈┈┈┈┈┃┃┈┈╰┫ ┣━E━╢
+ *             ╢━O━┣ ╰━┫┈┈┈┈┈╰╯╰┳━╯ ┣━Y━╢
+ *             ╢━O━┣ ┊┊┃┏┳┳━━┓┏┳┫┊┊ ┣━N━╢
+ *             ╨━━━┻━━━┗┛┗┛━━┗┛┗┛━━━┻━━━╨
+ * ```
+ */
+export class http_client {
+  public static s_instance: http_client = new http_client();
+  private constructor() {}
+}
