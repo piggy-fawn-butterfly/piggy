411e332626a5cbdc583782eab0fc94775f174e3e
jiangliang <jl88744653@gmail.com>
2020-03-09 01:52:46 +0800

    [modify] server/app.js 添加注释

--- piggy-core/server/app.js
+++ piggy-core/server/app.js
@@ -4,15 +4,21 @@ const wss_router = require("./wss-router");
 const { cat } = require("./common/functions");
 const { CA, PORT } = require("./config/constants");
 
-//创建服务器选项
+//HTTPS服务器选项
 const server_options = {
   key: cat(CA.KEY),
   cert: cat(CA.CRT),
   passphrase: CA.PASS
 };
-const piggy_app = koa_websocket(new koa_app(), {}, server_options);
-piggy_app.clients = {};
-piggy_app.listen(PORT.WSS);
-piggy_app.ws.use(wss_router.routes(), wss_router.allowedMethods());
 
-module.exports = piggy_app;
+//使用WebSocket代理
+const server = koa_websocket(new koa_app(), {}, server_options);
+server.listen(PORT.WSS);
+
+//记录所有连接的客户端
+server.clients = {};
+
+//路由分发处理
+server.ws.use(wss_router.routes(), wss_router.allowedMethods());
+
+module.exports = server;
